c26f1216890603467622f682001a4a1f5ffb5c1d
RaghavAgg0310 <49618756+RaghavAgg0310@users.noreply.github.com>
2020-11-28 00:23:07 +0530

    Add files via upload

--- soc.py
+++ soc.py
@@ -1,13 +1,44 @@
 import socket
 import sys
 import json
+import threading
+import time
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s = socket.socket()
-port = 5000
-s.connect(("localhost", port))
-while 1:
-    object = json.loads(s.recv(2048))
-    print(object)
-s.close() 
+sem = threading.Semaphore()
+LIST=[]
 
+def rec_request():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ("localhost", 5000)
+    sock.bind(server_address)
+    sock.listen(1)
+
+    while 1:
+        connection , address = sock.accept()
+        sem.acquire()
+        data = connection.recv(2048)
+        if data==b'':
+            sem.release()
+            break
+        obj = json.loads(data.decode("utf-8"))
+        LIST.append(obj)
+        sem.release()
+        print(obj)
+    connection.close()
+
+rec_request()
+print('hello')
+def display():
+    sem.release()
+    print(*LIST)
+    sem.acquire()
+"""
+thread1 = threading.Thread(target = rec_request)
+thread1.start()
+
+thread2 = threading.Thread(target = display)
+thread2.start()
+
+thread1.join()
+thread2.join()
+"""
