e03fc3656b164c5517351220b6d8447409ff4aec
shaashwatjain <shaashjain213@gmail.com>
2020-12-04 13:07:02 +0530

    updated master with scheduling inside it commented. Added requests_eval.py

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ gnome-terminal -- /bin/sh -c 'python3 master.py config.json RR'
 gnome-terminal -- /bin/sh -c 'python3 worker.py 4000 1'
 gnome-terminal -- /bin/sh -c 'python3 worker.py 4001 2'
 gnome-terminal -- /bin/sh -c 'python3 worker.py 4002 3'
-python3 requests.py 10
+python3 requests_eval.py 10
 echo "All running"
 
